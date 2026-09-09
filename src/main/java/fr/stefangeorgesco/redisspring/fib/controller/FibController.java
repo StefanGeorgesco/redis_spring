ae37d0bacde1034b9cd9abad8c57e237d9ac7c84
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-09 16:41:32 +0200

    add cache on FibService.getFib

---
+++ src/main/java/fr/stefangeorgesco/redisspring/fib/controller/FibController.java
@@ -0,0 +1,24 @@
+package fr.stefangeorgesco.redisspring.fib.controller;
+
+import fr.stefangeorgesco.redisspring.fib.service.FibService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("fib")
+public class FibController {
+
+    private final FibService service;
+
+    public FibController(FibService service) {
+        this.service = service;
+    }
+
+    @GetMapping("{n}")
+    public Mono<Integer> getFib(@PathVariable int n) {
+        return Mono.fromSupplier(() -> service.getFib(n));
+    }
+}
--- src/test/java/fr/stefangeorgesco/redisspring/TestcontainersConfiguration.java
+++ src/test/java/fr/stefangeorgesco/redisspring/TestcontainersConfiguration.java
@@ -4,6 +4,8 @@ import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -13,5 +15,27 @@ class TestcontainersConfiguration {
     @ServiceConnection
     RedisContainer redisContainer() {
         return new RedisContainer(DockerImageName.parse("redis:latest"));
+    }
+
+    @Bean
+    @SuppressWarnings("resource")
+    GenericContainer<?> cityApiExternalServicesContainer() {
+        return new GenericContainer<>(
+                DockerImageName.parse("city-api-external-services:latest")
+        )
+                .withExposedPorts(3030);
+    }
+
+    @Bean
+    @SuppressWarnings("HttpUrlsUsage")
+    DynamicPropertyRegistrar cityApiProperties(
+            GenericContainer<?> cityApiExternalServicesContainer) {
+
+        return registry -> registry.add(
+                "city.api.base-url",
+                () -> String.format("http://%s:%d/open-city-api",
+                        cityApiExternalServicesContainer.getHost(),
+                        cityApiExternalServicesContainer.getMappedPort(3030))
+        );
     }
 }
