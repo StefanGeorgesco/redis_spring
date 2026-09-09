582dc835cda32b5bec627038974e05f75fc1b32b
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-09 17:23:49 +0200

    add cache evict

--- src/main/java/fr/stefangeorgesco/redisspring/fib/controller/FibController.java
+++ src/main/java/fr/stefangeorgesco/redisspring/fib/controller/FibController.java
@@ -1,10 +1,7 @@
 package fr.stefangeorgesco.redisspring.fib.controller;
 
 import fr.stefangeorgesco.redisspring.fib.service.FibService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,5 +17,10 @@ public class FibController {
     @GetMapping("{n}/{name}")
     public Mono<Integer> getFib(@PathVariable int n, @PathVariable String name) {
         return Mono.fromSupplier(() -> service.getFib(n, name));
+    }
+
+    @PostMapping("{n}/clear")
+    public Mono<Void> clearCache(@PathVariable int n) {
+        return Mono.fromRunnable(() -> service.clearCache(n));
     }
 }
--- src/main/java/fr/stefangeorgesco/redisspring/fib/service/FibService.java
+++ src/main/java/fr/stefangeorgesco/redisspring/fib/service/FibService.java
@@ -2,6 +2,7 @@ package fr.stefangeorgesco.redisspring.fib.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,17 @@ public class FibService {
 
     private static final Logger log = LoggerFactory.getLogger(FibService.class);
 
+    // GET
     @Cacheable(value="math:fib", key = "#n")
     public int getFib(int n, String name) {
         log.info("Calculating Fibonacci number for n={}, name={}", n, name);
         return fib(n);
+    }
+
+    // Would be POST / PUT / PATCH / DELETE
+    @CacheEvict(value="math:fib", key = "#n")
+    public void clearCache(int n) {
+        log.info("Clearing Fibonacci cache for n={}", n);
     }
 
     // Intentional 2^n complexity
