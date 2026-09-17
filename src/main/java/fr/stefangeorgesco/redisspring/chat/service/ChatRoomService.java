77c9a7f7e189c808507ea7e49f0d1dd9486320bf
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-17 15:57:35 +0200

    add chat history

--- src/main/java/fr/stefangeorgesco/redisspring/chat/service/ChatRoomService.java
+++ src/main/java/fr/stefangeorgesco/redisspring/chat/service/ChatRoomService.java
@@ -1,6 +1,7 @@
 package fr.stefangeorgesco.redisspring.chat.service;
 
 import org.jspecify.annotations.NullMarked;
+import org.redisson.api.RListReactive;
 import org.redisson.api.RTopicReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.client.codec.StringCodec;
@@ -32,15 +33,17 @@ public class ChatRoomService implements WebSocketHandler {
     public Mono<Void> handle(WebSocketSession session) {
         String room = getChatRoomName(session);
         RTopicReactive topic = client.getTopic(room, StringCodec.INSTANCE);
+        RListReactive<String> history = client.getList("history:" + room, StringCodec.INSTANCE);
 
         Mono<Void> inbound = session.receive()
                 .map(WebSocketMessage::getPayloadAsText)
-                .flatMap(topic::publish)
+                .flatMap(msg -> history.add(msg).then(topic.publish(msg)))
                 .doOnError(error -> log.error("Error while publishing message to topic: {}",
                         error.getMessage()))
                 .then();
 
         Flux<WebSocketMessage> outbound = topic.getMessages(String.class)
+                .startWith(history.iterator())
                 .map(session::textMessage)
                 .doOnError(error -> log.error("Error while receiving message from topic: {}",
                         error.getMessage()));
